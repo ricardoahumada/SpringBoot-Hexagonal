d162125d206c2d956b472713e44cc83c8bbc8657
rahumada <ricardo@enmotionvalue.com>
2019-08-04 17:47:55 +0200

    named beans

--- tareas-proyectos-hexagonal/application/src/main/java/es/bit/tareasproyectoshex/configuration/ApplicationConfiguration.java
+++ tareas-proyectos-hexagonal/application/src/main/java/es/bit/tareasproyectoshex/configuration/ApplicationConfiguration.java
@@ -1,6 +1,7 @@
 package es.bit.tareasproyectoshex.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,7 @@ import es.bit.tareasproyectoshex.serviceadapters.UserServiceAdapter;
 public class ApplicationConfiguration {
 
     @Autowired
+    @Qualifier("jpaRepo")
     UserRepository userRepository;
 
     @Bean
